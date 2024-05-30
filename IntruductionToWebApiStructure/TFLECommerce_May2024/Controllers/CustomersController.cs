8c6c06799cb8923153df9f548e31c773c26fb7b5
shri <patilshridhar8056@gmail.com>
2024-05-30 23:17:07 +0530

    mvc example 2

---
+++ IntruductionToWebApiStructure/TFLECommerce_May2024/Controllers/CustomersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TFLECommerce_May2024.Models;
+
+namespace TFLECommerce_May2024.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Customer> customers = new List<Customer>();
+            customers.Add(new Customer { Id = 1, Name = "Ram", Email = "ram@gmail.com", Phone = "8596120452" });
+            customers.Add(new Customer { Id = 2, Name = "Sham", Email = "Sham@gmail.com", Phone = "75103649520" });
+            return View(customers);
+        }
+    }
+}
