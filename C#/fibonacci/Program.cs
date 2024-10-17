70b65a3084ab3cfc54e130dc58cc88426a8d0748
shri <patilshridhar8056@gmail.com>
2024-10-17 18:42:02 +0530

    fibo

---
+++ C#/fibonacci/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int n1 = 0;
+ int n2 = 1;
+ Console.WriteLine("Enter Number : ");
+ int number=int.Parse(Console.ReadLine());
+ Console.Write(n1+" "+n2+" ");
+ for (int i = 2; i<number; i++)
+{
+    int n3 = n1 + n2;
+    Console.Write(n3 + " ");
+    n1= n2;
+    n2= n3;
+}
